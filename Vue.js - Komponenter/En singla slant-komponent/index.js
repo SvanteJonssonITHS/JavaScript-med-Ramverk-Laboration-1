eae8dc239e376f695f623ee5b773bd75014b298f
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-12-22 09:53:45 +0100

    En singla slant-komponent

---
+++ Vue.js - Komponenter/En singla slant-komponent/index.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({});
+
+app.component('coin-toss', {
+	data() {
+		return { oneOrZero: Math.round(Math.random()) };
+	},
+	template: '<span v-if="oneOrZero == 0">Krona</span><span v-else>Klave</span>'
+});
+
+app.mount('#app');
