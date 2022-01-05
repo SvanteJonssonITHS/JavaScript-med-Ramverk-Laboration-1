9c7c09e79f1c0360c16141e00417d38e751508c8
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-01-05 09:48:02 +0100

    Programmatisk navigation

---
+++ Vue.js - Vue Router/Programmatisk navigation/index.js
@@ -0,0 +1,38 @@
+const app = Vue.createApp({});
+
+const Home = {
+	template: '<h1>Home</h1><input type="button" value="Page 1" @click="onClick">',
+	methods: {
+		onClick() {
+			this.$router.push('/page-1');
+		}
+	}
+};
+
+const Page1 = { template: '<h1>Page 1</h1>' };
+
+const Page2 = { template: '<h1>Page 2</h1>' };
+
+const routes = [
+	{
+		component: Home,
+		path: '/'
+	},
+	{
+		component: Page1,
+		path: '/page-1'
+	},
+	{
+		component: Page2,
+		path: '/page-2'
+	}
+];
+
+const router = VueRouter.createRouter({
+	history: VueRouter.createWebHashHistory(),
+	routes
+});
+
+app.use(router);
+
+app.mount('#app');
